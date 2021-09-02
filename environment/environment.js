c3ae6715c70231d2a3ec3e70359a4bf956e1af55
Anass 'Serdok' Lahnin <l.anass.pro@gmail.com>
2021-09-02 17:46:28 +0200

    Initial setup

---
+++ environment/environment.js
@@ -0,0 +1,11 @@
+export const environment = {
+    firebase: {
+        apiKey: "AIzaSyBp22MHE-ItsSd-VsPNk_IVd7NkMdFyEKU",
+        authDomain: "ciboulette-royale.firebaseapp.com",
+        projectId: "ciboulette-royale",
+        storageBucket: "ciboulette-royale.appspot.com",
+        messagingSenderId: "281693028745",
+        appId: "1:281693028745:web:ff6aadb0e55b29181f6158",
+        measurementId: "G-EXWS0R1V8C"
+    }
+};
