4bb0bd454aead0ebb7a54f0aea040bf49a898ff7
Anass 'Serdok' Lahnin <l.anass.pro@gmail.com>
2021-09-02 18:31:34 +0200

    Ajout Routing

--- App.js
+++ App.js
@@ -1,14 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Router, Route, Link } from './components/routing/routing';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  
+  constructor() {
+    super()
+    this.state= {
+      // Variables d'etats ici 
+    }
+  }
+  
+  render() {
+    
+    return (
+        <>
+          <StatusBar/>
+          <Router>      
+            <View style={styles.container}>
+            <Text>Open up App.js to start working on your app!</Text>
+            {/* <Route exact path="/" component={} */}
+            </View>
+
+          </Router>
+        </>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
