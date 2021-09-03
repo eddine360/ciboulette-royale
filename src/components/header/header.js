b5bcc02161889872c1f3aae7d07f820fa9d28227
Novax <damien.petitjean@laposte.net>
2021-09-03 10:46:09 +0200

    Affichage Schématique boutons

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
 
 
 export default class App extends React.Component {
@@ -12,11 +12,17 @@ export default class App extends React.Component {
 
     render() {
         return (
-            <>
-                <View>
-                    <Text>Test</Text>
+                <View style={styles.navBarBg}>
+                    <View style={styles.beginEndNavBar}>
+                      <Text style={styles.textParam}>Menu Burger</Text>
+                    </View>
+                    <Text style={{flex:4}}></Text>
+                    <View style={styles.beginEndNavBar}>
+                      <Text style={styles.textParam}>logo ?</Text>
+                    </View>
+
                 </View>
-            </>
+
         );
     }
 
@@ -24,6 +30,27 @@ export default class App extends React.Component {
 }
 
 
-const styles = StyleSheet.create({
+const textColor = {
+  color:'white'
+}
 
+const styles = StyleSheet.create({
+  navBarBg: {
+    flexDirection: 'row',
+    backgroundColor: '#383838',
+    justifyContent: 'space-between',
+    height: 70,
+  },
+  beginEndNavBar: {
+    flex:1,
+    alignContent: 'center',
+    textAlign:'center',
+    justifyContent:'center',
+    alignItems: 'center',
+    margin: 5
+  },
+  textParam: {
+    ...textColor,
+    fontWeight: 'bold'
+  }
 });
